eb8b15513e63b8ef78a4da346dbda9d0f23a91b0
sayyidan-i <142298634+sayyidan-i@users.noreply.github.com>
2024-01-06 08:56:54 +0700

    Delete opencv/opencv-3.4.16 directory
    delete soalnya bikin eror

--- opencv/opencv-3.4.16/build.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get install build-essential libgtk2.0-dev libavcodec-dev libavformat-dev libjpeg.dev libtiff4.dev libswscale-dev libjasper-dev
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
-sudo make
-sudo make install
-sudo gedit /etc/ld.so.conf.d/opencv.conf
